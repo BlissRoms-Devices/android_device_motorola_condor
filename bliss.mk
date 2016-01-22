0522dbb06076f3ce4cd45a7507c54d165ce1b9a2
Vatsal <koradiavatsal@hotmail.com>
2016-01-22 05:19:17 -0500

    Bootanim flags

--- bliss.mk
+++ bliss.mk
@@ -6,3 +6,8 @@ $(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := bliss_condor
 PRODUCT_RELEASE_NAME := MOTO E
+# Bootanimation
+TARGET_SCREEN_HEIGHT := 960
+TARGET_SCREEN_WIDTH := 540
+PRODUCT_COPY_FILES += \
+     vendor/bliss/prebuilt/common/bootanimation/600.zip:system/media/bootanimation.zip
